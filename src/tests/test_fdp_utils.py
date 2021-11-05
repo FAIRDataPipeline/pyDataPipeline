b3a6f9971841d3689c02595be55ac2ffd68719fb
Ryan J Field <RyanJField@users.noreply.github.com>
2021-11-05 06:56:19 +0000

    Refactor Tests

--- src/tests/test_fdp_utils.py
+++ src/tests/test_fdp_utils.py
@@ -3,12 +3,23 @@
 import pytest
 import os
 import datetime
+import platform
 import org.fairdatapipeline.api.common.fdp_utils as fdp_utils
 
-test_dir = os.path.join(os.path.dirname(__file__), "ext")
+@pytest.fixture
+def test_dir():
+    return os.path.join(os.path.dirname(__file__), "ext")
+
+@pytest.fixture
+def read_csv_path(test_dir):
+    return os.path.join(test_dir, 'read_csv.yaml')
+    
+@pytest.fixture
+def write_csv_path(test_dir):
+    return os.path.join(test_dir, 'write_csv.yaml')
 
 # Test is_file()
-def test_is_file_exists():
+def test_is_file_exists(test_dir):
     test_file = os.path.join(test_dir, 'test.csv')
     assert fdp_utils.is_file(test_file)
 
@@ -20,11 +31,9 @@ def test_is_file_exists():
 def test_is_file_not_exists(file_path):
     assert not fdp_utils.is_file(file_path)
 
-@pytest.mark.parametrize('file_path', [
-    os.path.join(test_dir, 'read_csv.yaml'),
-    os.path.join(test_dir, 'write_csv.yaml')
-])
-def test_is_yaml(file_path):
+@pytest.mark.parametrize('file_path', ["read_csv_path", "write_csv_path"])
+def test_is_yaml(file_path, request):
+    file_path = request.getfixturevalue(file_path)
     assert fdp_utils.is_yaml(file_path)
 
 @pytest.mark.parametrize('file_path', [
@@ -36,11 +45,9 @@ def test_is_yaml(file_path):
 def test_is_yaml_not(file_path):
     assert not fdp_utils.is_yaml(file_path)
 
-@pytest.mark.parametrize('file_path', [
-    os.path.join(test_dir, 'read_csv.yaml'),
-    os.path.join(test_dir, 'write_csv.yaml')
-])
-def test_is_valid_yaml(file_path):
+@pytest.mark.parametrize('file_path', ["read_csv_path", "write_csv_path"])
+def test_is_valid_yaml(file_path, request):
+    file_path = request.getfixturevalue(file_path)
     assert fdp_utils.is_yaml(file_path)
 
 @pytest.mark.parametrize('file_path', [
@@ -52,23 +59,28 @@ def test_is_valid_yaml(file_path):
 def test_is_valid_yaml_not(file_path):
     assert not fdp_utils.is_valid_yaml(file_path)
 
-def test_read_token():
+def test_read_token(test_dir):
     token = os.path.join(test_dir, 'test_token')
     assert fdp_utils.read_token(token) == "1a2b3c4d5e6d7f8a9b8c7d6e5f1a2b3c4d5e6d7f"
 
-def test_get_token():
+def test_get_token(test_dir):
     token = os.path.join(test_dir, 'test_token')
     assert fdp_utils.get_token(token) == "1a2b3c4d5e6d7f8a9b8c7d6e5f1a2b3c4d5e6d7f"
 
-def test_read_token_get_token():
+def test_read_token_get_token(test_dir):
     token = os.path.join(test_dir, 'test_token')
     assert fdp_utils.read_token(token) == fdp_utils.get_token(token)
 
-token = fdp_utils.read_token(os.path.join(os.path.expanduser('~'), '.fair/registry/token'))
+@pytest.fixture
+def token():
+    return fdp_utils.read_token(os.path.join(os.path.expanduser('~'), '.fair/registry/token'))
 
-def test_get_file_hash():
+def test_get_file_hash(test_dir):
     file_path = os.path.join(test_dir, 'test.csv')
-    assert fdp_utils.get_file_hash(file_path) == '51345410c236d375ccf47149196746bc7f4db29d'
+    if platform.system() == 'Windows':
+        assert fdp_utils.get_file_hash(file_path) == '1f71db9d999ded6d15c82a49b3ad7472cdcb19aa'
+    else:
+        assert fdp_utils.get_file_hash(file_path) == '51345410c236d375ccf47149196746bc7f4db29d'
 
 def test_random_hash_is_string():
     assert type(fdp_utils.random_hash()) == str
@@ -82,7 +94,7 @@ def test_extract_id():
 def test_get_headers():
     assert type(fdp_utils.get_headers()) == dict
 
-def test_get_headers_with_token():
+def test_get_headers_with_token(token):
     headers = fdp_utils.get_headers(token = token)
     assert headers['Authorization'] == 'token ' + token
 
@@ -94,12 +106,24 @@ def test_get_headers_api_version():
     headers = fdp_utils.get_headers(api_version= '0.0.1')
     assert headers['Accept'] == 'application/json; version=0.0.1'
 
-storage_root = None
-storage_root2 = None
-url = 'http://localhost:8000/api'
+@pytest.fixture
+def url():
+    if platform.system() == 'Windows':
+        return 'http://127.0.0.1:8000/api'
+    return 'http://localhost:8000/api'
 
-def test_post_entry():
-    global storage_root
+@pytest.fixture
+def storage_root_test(token, url, scope="module"):
+    return fdp_utils.post_entry(
+        token = token,
+        url= url,
+        data={
+            'root': 'https://storage-root-test.com'
+        },
+        endpoint= 'storage_root'
+    )
+
+def test_post_entry(token, url):
     storage_root = fdp_utils.post_entry(
         token = token,
         url= url,
@@ -110,9 +134,8 @@ def test_post_entry():
     )
     assert type(storage_root) == dict
 
-def test_post_entry_409():
-    global storage_root2
-    storage_root2 = fdp_utils.post_entry(
+def test_post_entry_409(token, url):
+    storage_root = fdp_utils.post_entry(
         token = token,
         url= url,
         data={
@@ -120,12 +143,28 @@ def test_post_entry_409():
         },
         endpoint= 'storage_root'
     )
-    assert type(storage_root2) == dict
+    assert type(storage_root) == dict
 
-def test_post_entry_equal():
-    assert storage_root == storage_root2
+def test_post_entry_equal(token, url):
+    storage_root = fdp_utils.post_entry(
+        token = token,
+        url= url,
+        data={
+            'root': 'https://test_2.com'
+        },
+        endpoint= 'storage_root'
+    )
+    storage_root_2 = fdp_utils.post_entry(
+        token = token,
+        url= url,
+        data={
+            'root': 'https://test_2.com'
+        },
+        endpoint= 'storage_root'
+    )
+    assert storage_root == storage_root_2
 
-def test_post_entry_500():
+def test_post_entry_500(token, url):
     with pytest.raises(Exception):
         fdp_utils.post_entry(
             token = token,
@@ -136,25 +175,25 @@ def test_post_entry_500():
             endpoint= 'non_existant'
         )
 
-def test_get_entry():
+def test_get_entry(url, token, storage_root_test):
     entry = fdp_utils.get_entry(
         url = url,
         query= {
-            'root': 'https://test.com'
+            'root': 'https://storage-root-test.com'
         },
         token = token,
         endpoint= 'storage_root'
     )
-    assert entry[0] == storage_root
+    assert entry[0] == storage_root_test
 
-def test_get_entity():
+def test_get_entity(url, storage_root_test):
     entity = fdp_utils.get_entity(
         url = url,
         endpoint='storage_root',
-        id = fdp_utils.extract_id(storage_root['url']))
-    assert entity == storage_root
+        id = fdp_utils.extract_id(storage_root_test['url']))
+    assert entity == storage_root_test
 
-def test_wrong_api_version():
+def test_wrong_api_version(token, url):
     with pytest.raises(Exception):
         fdp_utils.post_entry(
         token = token,
@@ -166,7 +205,7 @@ def test_wrong_api_version():
         api_version= '2.2.2'
         )
 
-def test_wrong_api_version_get():
+def test_wrong_api_version_get(token, url):
     with pytest.raises(Exception):
         fdp_utils.get_entry(
         token = token,
@@ -178,65 +217,75 @@ def test_wrong_api_version_get():
         api_version= '3.0.0'
         )
 
-def test_get_entity_with_token():
+def test_get_entity_with_token(url, storage_root_test, token):
     entity = fdp_utils.get_entity(
         url = url,
         endpoint= 'storage_root',
-        id = fdp_utils.extract_id(storage_root['url']),
+        id = fdp_utils.extract_id(storage_root_test['url']),
         token = token)
-    assert entity == storage_root
+    assert entity == storage_root_test
 
-def test_get_entity_non_200():
+def test_get_entity_non_200(url, storage_root_test):
     with pytest.raises(Exception):
         fdp_utils.get_entity(
             url = url,
             endpoint='non_existant',
-            id = fdp_utils.extract_id(storage_root['url']))
+            id = fdp_utils.extract_id(storage_root_test['url']))
 
-model_config = fdp_utils.post_entry(
-    url = url,
-    endpoint= 'object',
-    data={
-        'description': 'Model Config Test'
-    },
-    token = token
-)
+@pytest.fixture
+def model_config(url, token, scope="module"):
+    return fdp_utils.post_entry(
+        url = url,
+        endpoint= 'object',
+        data={
+            'description': 'Model Config Test'
+        },
+        token = token
+    )
 
-submission_script = fdp_utils.post_entry(
-    url = url,
-    endpoint= 'object',
-    data={
-        'description': 'Submission Script Test'
-    },
-    token = token
-)
+@pytest.fixture
+def submission_script(url, token, scope="module"):
+    return fdp_utils.post_entry(
+        url = url,
+        endpoint= 'object',
+        data={
+            'description': 'Submission Script Test'
+        },
+        token = token
+    )
 
-input_1 = fdp_utils.post_entry(
-    url = url,
-    endpoint= 'object',
-    data={
-        'description': 'Input 1'
-    },
-    token = token
-)
+@pytest.fixture
+def input_1(url, token, scope="module"):
+    return fdp_utils.post_entry(
+        url = url,
+        endpoint= 'object',
+        data={
+            'description': 'Input 1'
+        },
+        token = token
+    )
 
-input_1_component = input_1['components'][0]
+@pytest.fixture
+def input_1_component(input_1):
+    return input_1['components'][0]
 
-code_run = fdp_utils.post_entry(
-    url = url,
-    endpoint= 'code_run',
-    data={
-        'description': 'Test Code Run',
-        'run_date': str(datetime.datetime.now()),
-        'model_config': model_config['url'],
-        'submission_script': submission_script['url'],
-        'input_urls': [],
-        'output_urls': []
-    },
-    token = token
-)
+@pytest.fixture
+def code_run(url, model_config, submission_script, token, scope="module"):
+    return fdp_utils.post_entry(
+        url = url,
+        endpoint= 'code_run',
+        data={
+            'description': 'Test Code Run',
+            'run_date': str(datetime.datetime.now()),
+            'model_config': model_config['url'],
+            'submission_script': submission_script['url'],
+            'input_urls': [],
+            'output_urls': []
+        },
+        token = token
+    )
 
-def test_patch_entry():
+def test_patch_entry(code_run, input_1_component, token, url):
     fdp_utils.patch_entry(
         url = code_run['url'],
         data = {
@@ -251,7 +300,7 @@ def test_patch_entry():
     )
     assert input_1_component in code_run_updated['inputs']
 
-def test_patch_entry_non_200():
+def test_patch_entry_non_200(url, token):
     with pytest.raises(Exception):
         fdp_utils.patch_entry(
             url = url + '/api/users/1',
@@ -261,7 +310,7 @@ def test_patch_entry_non_200():
             token = token,
         )
 
-def test_post_storage_root_with_local():
+def test_post_storage_root_with_local(url, token):
     storage_root = fdp_utils.post_storage_root(
         token = token,
         url= url,
--- src/tests/test_raise_issue.py
+++ src/tests/test_raise_issue.py
@@ -4,60 +4,70 @@ import os
 import org.fairdatapipeline.api.common.fdp_utils as fdp_utils
 import pytest
 import shutil
+import platform
 
 from org.fairdatapipeline.api.common.raise_issue import raise_issue_by_data_product
-test_dir = os.path.join(os.path.dirname(__file__), "ext")
 
-token = fdp_utils.read_token(os.path.join(os.path.expanduser('~'), '.fair/registry/token'))
-script = os.path.join(test_dir, 'test_script.sh')
-config = os.path.join(test_dir, 'write_csv.yaml')
+@pytest.fixture
+def test_dir():
+    return os.path.join(os.path.dirname(__file__), "ext")
 
-handle = None
+@pytest.fixture
+def token():
+    return fdp_utils.read_token(os.path.join(os.path.expanduser('~'), '.fair/registry/token'))
 
-def test_initialise():
-    global handle
+@pytest.fixture
+def script(test_dir):
+    return os.path.join(test_dir, 'test_script.sh')
+
+@pytest.fixture
+def config(test_dir):
+    return os.path.join(test_dir, 'write_csv.yaml')
+
+def test_initialise(token, config, script):
     handle = pipeline.initialise(token, config, script)
     assert type(handle) == dict
 
-link_write = None
-
-def test_link_write():
-    global handle
-    global link_write
-    link_write = pipeline.link_write(handle, 'test/csv')
-    #print(handle)
+def test_link_write(token, config, script):
+    handle = pipeline.initialise(token, config, script)
+    pipeline.link_write(handle, 'test/csv')
     assert handle['output']['output_0']['data_product'] == 'test/csv'
 
-def test_raise_issue_by_index():
-    global handle
+def test_raise_issue_by_index(token, config, script):
+    handle = pipeline.initialise(token, config, script)
+    link_write = pipeline.link_write(handle, 'test/csv')
     index = pipeline.get_handle_index_from_path(handle, link_write)
     pipeline.raise_issue_by_index(handle, index, 'Test Issue', 7)
     assert handle['issues']['issue_0']['use_data_product'] == 'test/csv'
 
-def test_raise_issue_with_config():
-    global handle
+def test_raise_issue_with_config(token, config, script):
+    handle = pipeline.initialise(token, config, script)
     pipeline.raise_issue_with_config(handle, 'Test Issue with config', 4)
-    assert handle['issues']['issue_1']['type'] == 'config'
+    assert handle['issues']['issue_0']['type'] == 'config'
 
-def test_raise_issue_with_github_repo():
-    global handle
+def test_raise_issue_with_github_repo(token, config, script):
+    handle = pipeline.initialise(token, config, script)
     pipeline.raise_issue_with_github_repo(handle, 'Test Issue with github_repo', 4)
-    assert handle['issues']['issue_2']['type'] == 'github_repo'
+    assert handle['issues']['issue_0']['type'] == 'github_repo'
 
-def test_raise_issue_with_script():
-    global handle
+def test_raise_issue_with_script(token, config, script):
+    handle = pipeline.initialise(token, config, script)
     pipeline.raise_issue_with_submission_script(handle, 'Test Issue with submission_script', 4)
-    #print(handle)
-    assert handle['issues']['issue_3']['type'] == 'submission_script'
+    assert handle['issues']['issue_0']['type'] == 'submission_script'
 
-def test_finalise():
-    global handle
+def test_link_read(token, config, script, test_dir):
+    handle = pipeline.initialise(token, config, script)
     tmp_csv = os.path.join(test_dir, 'test.csv')
+    link_write = pipeline.link_write(handle, 'test/csv')
     shutil.copy(tmp_csv, link_write)
     pipeline.finalise(token, handle)
-    #print(handle)
 
-def test_raise_issue_existing_data_product():
+    config = os.path.join(test_dir, 'read_csv.yaml')    
+    handle = pipeline.initialise(token, config, script)
+    link_read = pipeline.link_read(handle, 'test/csv')
+    assert type(link_read) == str
+
+def test_raise_issue_existing_data_product(token, config, script):
     handle = pipeline.initialise(token, config, script)
     pipeline.raise_issue_by_existing_data_product(handle,
     'test/csv',
@@ -67,7 +77,7 @@ def test_raise_issue_existing_data_product():
     5)
     pipeline.finalise(token, handle)
 
-def test_raise_issue_data_product_from_reads():
+def test_raise_issue_data_product_from_reads(token, script, test_dir):
     config = os.path.join(test_dir, 'read_csv.yaml')
     handle = pipeline.initialise(token, config, script)
     pipeline.raise_issue_by_data_product(handle,
@@ -77,10 +87,3 @@ def test_raise_issue_data_product_from_reads():
     'Problem with csv File : Test Issue # 3',
     5)
     pipeline.finalise(token, handle)
-
-def test_link_read():
-    config = os.path.join(test_dir, 'read_csv.yaml')
-    handle = pipeline.initialise(token, config, script)
-    link_read = pipeline.link_read(handle, 'test/csv')
-    #logging.info('link_read: {}'.format(link_read))
-    assert type(link_read) == str
